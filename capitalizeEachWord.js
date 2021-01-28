753fb2635da0a241fa5b92be076b0e1d78e63504
Gökhan Ünal <unal_gokhan@outlook.com>
2021-01-29 02:43:43 +0300

    captalizeEachWord

---
+++ capitalizeEachWord.js
@@ -0,0 +1,14 @@
+function capitalizeEachWord(str) {
+  const words = []
+  // info key point  -----------⬇
+  for (const word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return words.join(' ')
+}
+
+console.log(
+  capitalizeEachWord(
+    "It's not the strongest species that survive, nor the most intelligent, but the most responsive to change"
+  )
+)
