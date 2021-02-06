6256673cd45900891f8a6c65d42446d6a6244081
Gökhan Ünal <unal_gokhan@outlook.com>
2021-02-07 00:27:53 +0300

    Add maxChar

---
+++ maxChar.js
@@ -0,0 +1,30 @@
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+function maxChar(str) {
+  const charMap = {}
+  let max = 0
+  let maxChar = ''
+
+  for (const char of str) {
+    if (charMap[char]) {
+      charMap[char]++
+    } else {
+      charMap[char] = 1
+    }
+    console.log('charMap', charMap)
+  }
+
+  for (const char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char]
+      maxChar = char
+    }
+  }
+  return maxChar
+}
+
+console.log(maxChar('abccccccccdde'))
